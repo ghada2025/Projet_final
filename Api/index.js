b966c64ddf31952a9a712ceebe5a9146f4c8f2ab
ghada2025 <khenieneghada@gmail.com>
2025-05-08 00:31:24 +0200

    cors

--- Api/index.js
+++ Api/index.js
@@ -24,7 +24,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "liberty-school.vercel.app",
         credentials: true,
     }
 });
